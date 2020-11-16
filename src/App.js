df180b0c0e96fb126139bd2d4c70c0a45698e614
jessicarf18 <jessicarosalia678@gmail.com>
2020-11-16 17:30:46 -0300

    creating initial project structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from './components/Title';
+
+function App() {
+  return (
+    <>
+      <Title> Your Shopping List </Title>
+    </>
+  );
+}
+
+export default App;
