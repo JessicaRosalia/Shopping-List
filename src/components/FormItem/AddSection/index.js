32d976aa1597dcce43c0414f5fc4780e2343ad05
jessicarf18 <jessicarosalia678@gmail.com>
2020-11-19 15:22:17 -0300

    creatingsectionn remove and section add

---
+++ src/components/FormItem/AddSection/index.js
@@ -0,0 +1,13 @@
+import { Button } from "../../Button";
+import { LabelField } from "../LabelField";
+import { TextField } from "../TextField";
+
+export function Add({ value, onChangeAdd, addItem }) {
+    return (
+        <>
+            <LabelField value="Add new Item:" />
+            <TextField value={value} placeholder="Milk" onChange={onChangeAdd} />
+            <Button value="Add" onClick={addItem} />
+        </>
+    )
+}
---
+++ src/components/FormItem/RemoveSection/index.js
@@ -0,0 +1,14 @@
+import { Button } from '../../Button'
+import { LabelField } from '../LabelField'
+import { TextField } from '../TextField'
+import './style.css'
+
+export function Remove({ value, onChangeRemove, removeItem }) {
+    return (
+        <>
+            <LabelField value="Remove Item:" />
+            <TextField value={value} placeholder="Apple" onChange={onChangeRemove} />
+            <Button value="Remove" onClick={removeItem} />
+        </>
+    )
+}
